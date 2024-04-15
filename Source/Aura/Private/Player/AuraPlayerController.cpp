6cba4a9c05d893de9e887b8518051a180d530472
vyxul <stevendotran@gmail.com>
2024-04-15 11:21:13 -0500

    13. Highlight Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,18 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include <Interaction/EnemyInterface.h>
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -54,5 +62,70 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	{
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+/**
+* Gets the first object under a cursor and perform operations on it
+*/
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	// if no hit, return
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = CurrentActor;
+	CurrentActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/**
+	* Line trace from cursor. There are several scenarios:
+	*  A. LastActor is null && CurrentActor is null
+	*		- Do nothing
+	*  B. LastActor is null && CurrentActor is valid
+	*		- Call Highlight function on CurrentActor
+	*  C. LastActor is valid && CurrentActor is null
+	*		- Call UnHighlight function on LastActor
+	*  D. Both actors are valid, but LastActor != CurrentActor
+	*		- UnHighlight LastActor, Highlight CurrentActor
+	*  E. Both actors are valid, LastActor == CurrentActor
+	*		- Do nothing
+	*/
+
+	// Scenarios A and B
+	if (LastActor == nullptr)
+	{
+		// A
+		// dont need any code for this scenario
+
+		// B
+		if (CurrentActor != nullptr)
+		{
+			CurrentActor->HighlightActor();
+		}
+	}
+
+	// Scenarios C, D, E
+	else // LastActor is valid
+	{
+		// C
+		if (CurrentActor == nullptr)
+		{
+			LastActor->UnHighlightActor();
+		}
+
+		// D and E
+		else // CurrentActor is valid
+		{
+			// D
+			if (LastActor != CurrentActor)
+			{
+				LastActor->UnHighlightActor();
+				CurrentActor->HighlightActor();
+			}
+
+			// E
+			// dont need any code for this scenario
+		}
 	}
 }
