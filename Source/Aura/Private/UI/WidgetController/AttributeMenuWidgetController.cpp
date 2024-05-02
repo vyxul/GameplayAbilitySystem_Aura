116ab83cdb77695a5f21184890617f0159ffea8f
vyxul <stevendotran@gmail.com>
2024-05-01 21:54:30 -0500

    89. Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,16 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::AttributeChanged(const FAuraAttributeInfo& AttributeInfo) const
+{ 
+}
