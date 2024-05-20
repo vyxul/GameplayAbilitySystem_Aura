3be9adccb8ffc4c27a36fcd605b733b4cbd646e1
vyxul <stevendotran@gmail.com>
2024-05-20 12:13:35 -0500

    144. Implementing Block Chance

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
 // this struct is only for this cpp file so no need for prefix or keyword to expose to other classes
@@ -12,10 +13,12 @@ struct AuraDamageStatics
 	// using macros to easily declare and define AttributeCaptureDef variables, look at MMC classes for long form versions
 	// if needing same attribute from both target and source, maybe need to do it long form or make own macro
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	
 	AuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
 	}
 };
 
@@ -29,12 +32,13 @@ static const AuraDamageStatics& DamageStatics()
 UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
 	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
 {
-	/* Get Needed Info before Calculations */
+	/* Get Needed Info before Calculations / Boilerplate */
 	// Get ASC's
 	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
 	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
@@ -45,25 +49,41 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	// Get GE Spec
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
-
-	/* Getting attribute info */
+	
 	// create EvaluateParams needed for AttemptCalculateCapturedAttributeMagnitude()
 	FAggregatorEvaluateParameters EvaluateParameters;
 	EvaluateParameters.SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
 	EvaluateParameters.TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	/* End Boilerplate Code */
 
+	/* Get Damage Set by Caller Magnitude */
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+	
+	/* Getting attribute info */
 	// set up variables to capture attribute magnitudes
-	float Armor = 0.f;
+	//float Armor = 0.f;
+	// Capture BlockChance on Target, and determine if there was a successful block
+	// If Block, halve the damage.
+	float BlockChance = 0.f;
 	
 	// Call function to get attribute info
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	//ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters, BlockChance);
 
 	// clamp any attribute magnitudes as needed
 	// this is to clamp armor to be > 0, doing simple modifier for now for demo
-	Armor = FMath::Max<float>(0.f, Armor);
-	++Armor;
+	//Armor = FMath::Max<float>(0.f, Armor);
+	//++Armor;
+	BlockChance = FMath::Max<float>(0.f, BlockChance);
+
+	// Perform any calculations needed with the attribute magnitudes retrieved
+	float RandomNumber = FMath::FRand() * 100;
+	bool bAttackBlocked = RandomNumber <= BlockChance ? true : false;
+	if (bAttackBlocked)
+		Damage = Damage / 2;
 
 	/* Modify Attributes */
-	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	//const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
