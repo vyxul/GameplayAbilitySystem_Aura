a9e21853c7cfa35eb4bdeec0fdfefee47f7b1f28
vyxul <stevendotran@gmail.com>
2024-07-29 11:11:27 -0500

    327. Target Trace Channel

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
 #include "AbilitySystemComponent.h"
+#include "Aura/Aura.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -47,7 +48,7 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 	// Get the HitResult of cursor from PC
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
     FHitResult CursorHit;
-    PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+    PC->GetHitResultUnderCursor(ECC_Target, false, CursorHit);
 
 	// Prepare the target data to send to server
 	FGameplayAbilityTargetDataHandle DataHandle;
