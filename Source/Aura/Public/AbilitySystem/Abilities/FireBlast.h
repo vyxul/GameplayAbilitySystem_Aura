fd192fd71b8a7f0e2a71ad23f518ed451590b0f1
vyxul <stevendotran@gmail.com>
2024-08-10 11:00:47 -0500

    364. FireBlast Cost and Cooldown
    Made FireBlast cpp class, should just follow from tutorial

---
+++ Source/Aura/Public/AbilitySystem/Abilities/FireBlast.h
@@ -0,0 +1,17 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "FireBlast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UFireBlast : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
