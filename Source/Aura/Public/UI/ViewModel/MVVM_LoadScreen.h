6437f459b05043b55afbd6f9c4248870904b4ee2
vyxul <stevendotran@gmail.com>
2024-08-19 16:31:11 -0500

    383. Load Slot View Model

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -6,6 +6,7 @@
 #include "MVVMViewModelBase.h"
 #include "MVVM_LoadScreen.generated.h"
 
+class UMVVM_LoadSlot;
 /**
  * 
  */
@@ -13,5 +14,26 @@ UCLASS()
 class AURA_API UMVVM_LoadScreen : public UMVVMViewModelBase
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadSlot> LoadSlotViewModelClass;
 	
+	void InitializeLoadSlots();
+
+	UFUNCTION(BlueprintPure)
+	UMVVM_LoadSlot* GetLoadSlotViewModelByIndex(int32 Index);
+
+private:
+	UPROPERTY()
+	TMap<int32, UMVVM_LoadSlot*> LoadSlots;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_0;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_1;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_2;
 };
