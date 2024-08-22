215fc5e3762c6e64d00a2e91050f56994e2a7ac1
vyxul <stevendotran@gmail.com>
2024-08-22 17:14:42 -0500

    407. Data Structures for Saving Data

--- Source/Aura/Private/Game/AuraSaveGame.cpp
+++ Source/Aura/Private/Game/AuraSaveGame.cpp
@@ -3,3 +3,24 @@
 
 #include "Game/AuraSaveGame.h"
 
+FSavedMap UAuraSaveGame::GetSavedMapWithMapName(const FString& InMapName)
+{
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+			return Map;
+	}
+	
+	return FSavedMap();
+}
+
+bool UAuraSaveGame::HasMap(const FString& InMapName)
+{
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+			return true;
+	}
+	
+	return false;
+}
--- Source/Aura/Public/Game/AuraSaveGame.h
+++ Source/Aura/Public/Game/AuraSaveGame.h
@@ -46,6 +46,39 @@ inline bool operator== (const FSavedAbility& Left, const FSavedAbility& Right)
 	return Left.AbilityTag.MatchesTagExact(Right.AbilityTag);
 }
 
+USTRUCT(BlueprintType)
+struct FSavedActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FName ActorName = FName();
+
+	UPROPERTY()
+	FTransform Transform = FTransform();
+
+	// Serialized variables from the Actor - only those marked with SaveGame specifier
+	UPROPERTY()
+	TArray<uint8> Bytes;
+};
+
+inline bool operator== (const FSavedActor& Left, const FSavedActor& Right)
+{
+	return Left.ActorName == Right.ActorName;
+}
+
+USTRUCT(BlueprintType)
+struct FSavedMap
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FString MapAssetName = FString();
+
+	UPROPERTY()
+	TArray<FSavedActor> SavedActors;
+};
+
 /**
  * 
  */
@@ -105,4 +138,11 @@ public:
 	/* Abilities */
 	UPROPERTY()
 	TArray<FSavedAbility> SavedAbilities;
+
+	/* Map and Actors */
+	UPROPERTY()
+	TArray<FSavedMap> SavedMaps;
+
+	FSavedMap GetSavedMapWithMapName(const FString& InMapName);
+	bool HasMap(const FString& InMapName);
 };
